75e7b51944cdc9432843f8e0dc25e813e0abe5be
swfz <sawafuji.09@gmail.com>
2016-08-12 08:41:57 +0000

    WIP

--- sample_v2.js
+++ sample_v2.js
@@ -1,26 +1,22 @@
 exports.handle = function(event, context) {
   process.env.TZ = 'Asia/Tokyo';
   const config = require('config');
+  const util   = require('util');
 
-  const logger = (f,obj) => {
-    console.dir(f);
-    console.dir(obj);
+  const dump = (o) => {
+    util.log(util.inspect(o,{depth: 7}))
   }
 
   const pfunc = (err,data) => {
-    if (err) {
-      console.log(err,err.stack);
-    }
-    else {
-      console.log(data);
-    }
+    if (err) { util.log(err,err.stack); }
+    else     { util.log(data); }
   }
 
   Array.prototype.divide = function(n){
-    var array = this;
+    const array  = this;
+    const length = array.length;
     var index = 0;
     var dividedArray = [];
-    var length = array.length;
 
     while ( index < length ) {
       var partial = array.slice(index,index+n);
@@ -32,32 +28,33 @@ exports.handle = function(event, context) {
   }
 
   function makeAttachement(tweet) {
-    var posted     = Date.parse( tweet.created_at );
-    var postedDate = new Date(posted);
-    var postedLink = "https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str;
+    const posted     = Date.parse( tweet.created_at );
+    const postedDate = new Date(posted);
+    const postedLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
 
+    dump(tweet);
     // slackAPIのpayloadで使用するためsnake case
-    var attachment = {
+    const attachment = {
       color: "#334fa6",
       text: tweet.text,
       author_name: tweet.user.name,
-      author_link: "https://twitter.com/" + tweet.user.screen_name,
+      author_link: `https://twitter.com/${tweet.user.screen_name}`,
       author_icon: tweet.user.profile_image_url_https,
       fields: [{
-        title: "Posted: " + postedDate.toLocaleString(),
+        title: `Posted: ${postedDate.toLocaleString()}`,
         value: postedLink,
-        short: true
+        short: false
       }]
     }
 
     return attachment;
   }
 
-  function post2slack(attachments){
+  function post2Slack(attachments){
     return new Promise(function(resolve,reject){
-      var slackToken = config.slack.token;
-      var slackApiClient = require('slack-api-client');
-      var slack = new slackApiClient(slackToken);
+      const slackToken     = config.slack.token;
+      const slackApiClient = require('slack-api-client');
+      const slack          = new slackApiClient(slackToken);
 
       slack.api.chat.postMessage({
         channel: config.slack.channel,
@@ -65,12 +62,9 @@ exports.handle = function(event, context) {
         text: attachments.length + '/' + config.divideCount + 'tweets',
         attachments: JSON.stringify( attachments ),
       }, function (err,res){
-        if (err) {
-          console.log(err);
-          console.log(res);
-        };
-        var log = "posted " + attachments.length + " tweet. " + attachments[0].fields[0].title + " TO " + attachments[attachments.length - 1].fields[0].title;
-        console.log(log);
+        if (err) { util.log(err); util.log(res); };
+        const log = `posted ${attachments.length} tweet. ${attachments[0].fields[0].title} TO ${attachments[attachments.length - 1].fields[0].title}`;
+        util.log(log);
         resolve(log);
       });
     });
@@ -78,8 +72,8 @@ exports.handle = function(event, context) {
 
   function readPostedTweet(){
     return new Promise(function(resolve,reject){
-      var dynamo = dynamoClient();
-      var params = {
+      const dynamo = dynamoClient();
+      const params = {
         "TableName": config.dynamo.table,
         "Key": {
           "key": config.dynamo.key
@@ -96,64 +90,52 @@ exports.handle = function(event, context) {
         });
       });
 
-      asyncGetitem.then(function(posted){
-        resolve(posted);
-      });
+      asyncGetitem.then( posted => resolve(posted) )
     });
   }
 
-  function writePostedTweet( posted_tweet ){
-    var dynamo = dynamoClient();
+  function writePostedTweet( postedTweet ){
+    const dynamo = dynamoClient();
 
-    var params = {
+    const params = {
       TableName: config.dynamo.table,
       Item: {
         key: config.dynamo.key,
-        value: posted_tweet
+        value: postedTweet
       }
     };
 
     dynamo.putItem(params,pfunc);
   }
 
-  var dynamoClient = () => {
-    var AWS = require('aws-sdk');
+  const dynamoClient = () => {
+    const AWS = require('aws-sdk');
     if ( process.env.AWS_PROFILE ) {
       AWS.config.update({endpoint: config.dynamo.endpoint, region: config.dynamo.region });
     }
-    var doc = require('dynamodb-doc');
-    var dynamo = new doc.DynamoDB();
+    const doc    = require('dynamodb-doc');
+    const dynamo = new doc.DynamoDB();
 
     return dynamo;
   }
 
-  // const isAlreadyPosted = (posted,index,array) => {
-  //   logger('isAlreadyPosted',self);
-  //   self.id == posted.tweet_id;
-  // }
   function isAlreadyPosted(posted,index,array){
     return this.id == posted.tweet_id;
   }
 
-  function timeline2Slack(error,tweets,response){
-    if (error) {
-      console.log(error);
-      return;
-    }
+  function timeline2Slack(err,tweets,response){
+    if (err) { util.log(err); return; }
 
     var asyncReadPosted = readPostedTweet();
     asyncReadPosted.then(function(posted){
 
-      console.log('tweet count: ' + tweets.length);
-      var filteredTweets = tweets.filter(function(tweet,index,array){
-        if ( posted.length == 0 ){
-          return true;
-        }
-        // console.log( !posted.some(isAlreadyPosted, tweet) );
+      util.log(`tweet count: ${tweets.length}`);
+      const filteredTweets = tweets.filter(function(tweet,index,array){
+        if ( posted.length == 0 ){ return true; }
         return ( !posted.some(isAlreadyPosted, tweet) );
       });
 
-      console.log('filtered_tweet: ' + filteredTweets.length);
+      util.log(`filtered_tweet: ${filteredTweets.length}`);
 
       attachments = [];
       filteredTweets.forEach(function(tweet){
@@ -162,23 +144,18 @@ exports.handle = function(event, context) {
 
       divided_attachements = attachments.reverse().divide(config.divideCount);
       divided_attachements.reduce(function(promise,partialAttachements){
-        return promise.then(function(result){
-          return post2slack(partialAttachements);
-        });
+        return promise.then( result => post2Slack(partialAttachements) );
       },
         Promise.resolve()
       );
 
-      var posted_tweet = tweets.map(function(tweet, index, array){
-        return {tweet_id: tweet.id, posted: tweet.created_at};
-      });
-      writePostedTweet( JSON.stringify(posted_tweet) )
+      const postedTweet = tweets.map( (tweet, index, array) => ( {tweet_id: tweet.id, posted: tweet.created_at} ) );
+      writePostedTweet( JSON.stringify(postedTweet) )
       context.succeed( {"to slack tweets": filteredTweets.length} );
     });
   }
 
-  console.log('aaa');
-  var Twitter = require('twitter');
+  const Twitter = require('twitter');
   const twitterClient = new Twitter({
     consumer_key: config.twitter.consumer_key,
     consumer_secret: config.twitter.consumer_secret,
